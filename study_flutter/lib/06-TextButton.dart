82771fa7285590337cdeaba782a530ec64066611
nhuytran122 <nhuytran12223@gmail.com>
2024-09-15 08:52:35 +0700

    widget TextButton 2

--- study_flutter/lib/06-TextButton.dart
+++ study_flutter/lib/06-TextButton.dart
@@ -13,19 +13,31 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: TextButton(
+      child: TextButton.icon(
         onPressed: () {
           print('Click text button');
         },
+        //onPressed: null,
         style: TextButton.styleFrom(
           foregroundColor: Colors.blue,
           backgroundColor: Colors.pink,
           // minimumSize: Size(120, 60),
           padding: EdgeInsets.all(30),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          elevation: 20,
+          shadowColor: Colors.blue.withOpacity(0.5),
+          side: BorderSide(width: 2, color: Colors.orange),
+          // disabledBackgroundColor: Colors.grey,
+          // disabledForegroundColor: Colors.white,
         ),
-        child: Text(
+        label: Text(
           'Text Button',
           style: TextStyle(fontSize: 36.0),
+        ),
+        icon: Icon(
+          Icons.add,
+          size: 30,
         ),
       ),
     );
